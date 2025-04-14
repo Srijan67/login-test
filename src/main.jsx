e8f73628cb33c081f8670096a00dbefbc8f431c1
Srijan67 <srijansinha67@gmail.com>
2025-04-14 20:04:54 +0530

    background change to red

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import 'react-app-polyfill/ie9';
+import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
