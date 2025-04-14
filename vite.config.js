0213e2ce2fae90916fd044ed23bd3ac0a77c91f9
Srijan67 <srijansinha67@gmail.com>
2025-04-14 17:20:56 +0530

    reset vite config

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    allowedHosts: [
-      '5b33-160-202-37-188.ngrok-free.app'
-    ]
-  }
+  plugins: [react()]
 })
