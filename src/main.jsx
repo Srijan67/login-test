ae8f53789423ae0fc2d8d9e774e7f034454c2e5b
Srijan67 <srijansinha67@gmail.com>
2025-04-14 22:02:33 +0530

    testing

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,3 @@
-import 'react-app-polyfill/ie9';
-import 'react-app-polyfill/ie11';
-import 'react-app-polyfill/stable';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
