ba6fdd368c9d6f1e9f337ed467e3cbc902eda5e7
Srijan67 <srijansinha67@gmail.com>
2025-04-14 22:17:34 +0530

    testing

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react'; // if you're using this plugin for React
-import legacy from '@vitejs/plugin-legacy';
 
 export default defineConfig({
   plugins: [
     react(),
-    legacy({
-      targets: ['ie >= 11'],
-      // Optionally, include additional polyfills if needed:
-    })
   ]
 });
